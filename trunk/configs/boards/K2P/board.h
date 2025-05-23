127a64b08a7e30f58d36b7a77185dc1853ba62a7
TürBö <6028824+TurBoTse@users.noreply.github.com>
2025-05-24 00:07:10 +0800

    Update K2P

--- trunk/configs/boards/K2P/board.h
+++ trunk/configs/boards/K2P/board.h
@@ -1,21 +1,27 @@
-/* K2P */
+/* === PHICOMM K2P Wireless Router Board Definition === */
 
-#define BOARD_PID		"K2P"
-#define BOARD_NAME		"K2P"
-#define BOARD_DESC		"PHICOMM K2P Wireless Router"
-#define BOARD_VENDOR_NAME	"PHICOMM"
-#define BOARD_VENDOR_URL	"http://www.phicomm.com/"
-#define BOARD_MODEL_URL		"http://www.phicomm.com/"
-#define BOARD_BOOT_TIME		30
-#define BOARD_FLASH_TIME	120
+#define BOARD_PID               "K2P"
+#define BOARD_NAME              "K2P"
+#define BOARD_DESC              "PHICOMM K2P Wireless Router"
+#define BOARD_VENDOR_NAME       "PHICOMM"
+#define BOARD_VENDOR_URL        "http://www.phicomm.com/"
+#define BOARD_MODEL_URL         "http://www.phicomm.com/"
 
-#define BOARD_HAS_5G_11AC		1
-#define BOARD_NUM_ANT_5G_TX		2
-#define BOARD_NUM_ANT_5G_RX		2
-#define BOARD_NUM_ANT_2G_TX		2
-#define BOARD_NUM_ANT_2G_RX		2
-#define BOARD_HAS_EPHY_L1000	1
-#define BOARD_HAS_EPHY_W1000	1
-#define BOARD_NUM_ETH_LEDS		0
-#define BOARD_NUM_UPHY_USB3		0
-#define BOARD_USB_PORT_SWAP		0
+#define BOARD_BOOT_TIME         30      /* seconds */
+#define BOARD_FLASH_TIME        120     /* seconds */
+
+/* === Wireless Capabilities === */
+#define BOARD_HAS_5G_11AC       1
+#define BOARD_NUM_ANT_5G_TX     2
+#define BOARD_NUM_ANT_5G_RX     2
+#define BOARD_NUM_ANT_2G_TX     2
+#define BOARD_NUM_ANT_2G_RX     2
+
+/* === Ethernet & PHY === */
+#define BOARD_HAS_EPHY_L1000    1       /* LAN supports 1000 Mbps */
+#define BOARD_HAS_EPHY_W1000    1       /* WAN supports 1000 Mbps */
+#define BOARD_NUM_ETH_LEDS      0
+
+/* === USB Configuration === */
+#define BOARD_NUM_UPHY_USB3     0       /* No USB 3.0 ports */
+#define BOARD_USB_PORT_SWAP     0       /* USB port order not swapped */
--- trunk/configs/boards/K2P/board.mk
+++ trunk/configs/boards/K2P/board.mk
@@ -1,9 +1,13 @@
-##################################################################
-# Board PID # Board Name       # PRODUCT # Note
-##################################################################
-# K2P       # K2P              # MT7621  #
-##################################################################
+#####################################################################
+#  Board PID  |  Board Name     |  PRODUCT   |       Note
+#####################################################################
+#   K2P       |  K2P            |  MT7621    |  PHICOMM K2P with MT7615 DBDC
+#####################################################################
 
-CFLAGS += -DBOARD_K2P -DBOARD_MT7615_DBDC
-BOARD_NUM_USB_PORTS=0
-CONFIG_BOARD_RAM_SIZE=128
+# Compiler flags
+CFLAGS += -DBOARD_K2P 
+CFLAGS += -DBOARD_MT7615_DBDC
+
+# Hardware configuration
+BOARD_NUM_USB_PORTS   := 0           # USB port configuration
+CONFIG_BOARD_RAM_SIZE := 128         # RAM size in MB
