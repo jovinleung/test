4efe5730be71740d6604a5534e3a9bf1ad5f5c62
TürBö <6028824+TurBoTse@users.noreply.github.com>
2025-05-23 15:24:51 +0800

    Update g-ax1800

--- trunk/configs/boards/G-AX1800/board.h
+++ trunk/configs/boards/G-AX1800/board.h
@@ -1,22 +1,27 @@
-/* G-AX1800 */
+/* === G-AX1800 Wireless Router Board Definition === */
 
-#define BOARD_PID		"G-AX1800"
-#define BOARD_NAME		"G-AX1800"
-#define BOARD_DESC		"G-AX1800 Wireless Router"
-#define BOARD_VENDOR_NAME	"FCJ GROUP"
-#define BOARD_VENDOR_URL	"http://www.fcjjt.com/"
-#define BOARD_MODEL_URL		"http://www.fcjjt.com/"
-#define BOARD_BOOT_TIME		20
-#define BOARD_FLASH_TIME	120
+#define BOARD_PID               "G-AX1800"
+#define BOARD_NAME              "G-AX1800"
+#define BOARD_DESC              "G-AX1800 Wireless Router"
+#define BOARD_VENDOR_NAME       "FCJ GROUP"
+#define BOARD_VENDOR_URL        "http://www.fcjjt.com/"
+#define BOARD_MODEL_URL         "http://www.fcjjt.com/"
 
-#define BOARD_HAS_5G_11AC		1
-#define BOARD_HAS_5G_11AX		1
-#define BOARD_HAS_2G_11AX		1
-#define BOARD_NUM_ANT_5G_TX		2
-#define BOARD_NUM_ANT_5G_RX		2
-#define BOARD_NUM_ANT_2G_TX		2
-#define BOARD_NUM_ANT_2G_RX		2
-#define BOARD_NUM_ETH_LEDS		0
-#define BOARD_NUM_ETH_EPHY		4
-#define BOARD_HAS_EPHY_L1000	1
-#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_BOOT_TIME         20      /* Boot time in seconds */
+#define BOARD_FLASH_TIME        120     /* Flash write time in seconds */
+
+/* === Wireless Capabilities === */
+#define BOARD_HAS_5G_11AC       1       /* Supports 5GHz 802.11ac */
+#define BOARD_HAS_5G_11AX       1       /* Supports 5GHz 802.11ax (Wi-Fi 6) */
+#define BOARD_HAS_2G_11AX       1       /* Supports 2.4GHz 802.11ax (Wi-Fi 6) */
+#define BOARD_HAS_DBDC          1       /* Dual-Band Dual-Concurrent Support */
+#define BOARD_NUM_ANT_5G_TX     2
+#define BOARD_NUM_ANT_5G_RX     2
+#define BOARD_NUM_ANT_2G_TX     2
+#define BOARD_NUM_ANT_2G_RX     2
+
+/* === Ethernet Configuration === */
+#define BOARD_NUM_ETH_EPHY      4       /* 4 Ethernet PHY ports (LAN/WAN total) */
+#define BOARD_HAS_EPHY_L1000    1       /* LAN supports 1000 Mbps */
+#define BOARD_HAS_EPHY_W1000    1       /* WAN supports 1000 Mbps */
+#define BOARD_NUM_ETH_LEDS      0       /* No independent Ethernet LED indicators */
--- trunk/configs/boards/G-AX1800/board.mk
+++ trunk/configs/boards/G-AX1800/board.mk
@@ -1,9 +1,14 @@
 ##################################################################
-# Board PID # Board Name       # PRODUCT # Note
+# Board PID # Board Name       # PRODUCT        # Note
 ##################################################################
-# G-AX1800  # G-AX1800         # MT7621  #
+# G-AX1800  # G-AX1800         # MT7621, MT7915 # Wi-Fi 6, DBDC
 ##################################################################
 
-CFLAGS += -DBOARD_G_AX1800 -DBOARD_MT7915_DBDC
-BOARD_NUM_USB_PORTS=0
-CONFIG_BOARD_RAM_SIZE=256
+# Compiler flags
+CFLAGS += -DBOARD_G_AX1800          # Board identifier
+CFLAGS += -DBOARD_MT7915_DBDC       # MT7915 Wi-Fi chip with Dual Band Dual Concurrent
+CFLAGS += -DBOARD_HAS_DBDC          # Dual-Band Dual-Concurrent Support
+
+# Hardware configuration
+BOARD_NUM_USB_PORTS   := 0           # Number of USB ports (0 = none) 
+CONFIG_BOARD_RAM_SIZE := 256         # RAM size in MB
